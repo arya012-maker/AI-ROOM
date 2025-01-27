9430df417f4f8a52d226bd8fe53acec0465c46e0
arya012-maker <aryamann233@gmail.com>
2025-01-28 05:25:55 +0530

    Changed the DB URL

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./config/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_pZVNB6Fw9bUA@ep-ancient-rain-a4pgqd9v-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
 });
